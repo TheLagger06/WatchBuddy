e0dd23e5deee5ac198929e77b8785c5a4e69482f
Hugo Abreu <hrabreu06@gmail.com>
2024-10-04 15:21:37 +0000

    fix whitenoise and static files

--- tracker/settings.py
+++ tracker/settings.py
@@ -32,9 +32,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'watchlist', 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [ ".herokuapp.com"]
+ALLOWED_HOSTS = [ "watchbuddy.herokuapp.com"]
 
 
 # Application definition
@@ -132,9 +132,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For collected static files
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Where your static files (CSS/JS) are in the project
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
